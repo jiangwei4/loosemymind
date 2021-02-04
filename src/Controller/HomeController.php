96d94a5493c29ba83068eca4654b91425fb2bada
donovan <dono.rock@hotmail.fr>
2021-02-04 11:01:46 +0100

    tableau de bord

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PartieRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(PartieRepository $partieRepository): Response
     {
+        $user = $this->getUser();
+        $listePartie = $partieRepository->findBy(['user'=>$user]);
+        //dump($listePartie); die();
+        $tableauDeBord = [sizeof($listePartie),5,4];
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'tableauDeBord' => $tableauDeBord,
         ]);
     }
 }
