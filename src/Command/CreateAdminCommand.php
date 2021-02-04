400729b29f5e51a47c6ea3a5c13b20de5705c2b6
donovan <dono.rock@hotmail.fr>
2021-02-04 11:17:53 +0100

    admin commande

--- src/Command/CreateAdminCommand.php
+++ src/Command/CreateAdminCommand.php
@@ -9,15 +9,18 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class CreateAdminCommand extends Command
 {
-    protected static $defaultName = 'createAdmin';
+    protected static $defaultName = 'app:createAdmin';
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder)
     {
         $this->entityManager= $entityManager;
+        $this->encoder = $encoder;
         parent::__construct();
     }
 
@@ -29,6 +32,7 @@ class CreateAdminCommand extends Command
             ->addArgument('prenom', InputArgument::REQUIRED, 'prenom description')
             ->addArgument('email', InputArgument::REQUIRED, 'email description')
             ->addArgument('motdepasse', InputArgument::REQUIRED, 'password description')
+            ->addArgument('telephone', InputArgument::REQUIRED, 'telephone description')
         ;
     }
 
@@ -39,19 +43,23 @@ class CreateAdminCommand extends Command
         $firstname = $input->getArgument('prenom');
         $lastname = $input->getArgument('nom');
         $password = $input->getArgument('motdepasse');
-        if (null === $email || null === $firstname || null === $lastname ||null === $password) {
+        $telephone = $input->getArgument('telephone');
+        if (null === $email || null === $firstname || null === $lastname ||null === $password || null === $telephone) {
             $io->error('Vous n\'avez pas rentré d\'email et/ou de firstname et/ou lastname et/ou de mot de passe');
             return;
         }
-        $io->note(sprintf('Create a Master for email: %s, prenom: %s, nom: %s, password: %s', $email,$firstname,$lastname,$password));
-        $user = new Master();
-        $user->setEmail($email);
-        $user->setFirstname($prenom);
-        $user->setLastname($nom);
-        $user->setPassword($password);
+        $io->note(sprintf('Create a Admin User for email: %s, prenom: %s, nom: %s, password: %s, telephone: %s', $email,$firstname,$lastname,$password,$telephone));
+        $user = new User();
+        $user->setMail($email);
+        $user->setPrenom($firstname);
+        $user->setNom($lastname);
+        $passwordEncoded = $this->encoder->encodePassword($user, $password);
+        $user->setPassword($passwordEncoded);
+        $user->setTelephone($telephone);
         $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
         $this->entityManager->persist($user);
         $this->entityManager->flush();
-        $io->success(sprintf('You\'ve created an Admin-user with email: %s and firstname %s  and lastname %s', $email, $firstname,$lastname ));
+        $io->success(sprintf('You\'ve created an Admin-user with email: %s and password %s', $email, $password));
+        return 1;
     }
 }
